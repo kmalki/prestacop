0cb835530f7f5a1c762f8d2e13092ba052f2be10
hedi934 <58746931+hedi934@users.noreply.github.com>
2020-07-08 20:26:43 +0200

    Update main.scala

--- src/main/scala/csv/main.scala
+++ src/main/scala/csv/main.scala
@@ -1,10 +1,33 @@
-object Main {
+package csv
+import java.io.BufferedReader
+import java.io.FileReader
+
+import scala.annotation.tailrec
+import scala.io.Source
+
+object main {
   def main(args: Array[String]): Unit = {
 
-      val input = io.Source.fromFile("/home/hedi/IdeaProjects/drone/csv/project/Parking_Violations_Issued_-_Fiscal_Year_2015.csv").getLines()
+    val listFile = List("/home/hedi/IdeaProjects/drone/csv/project/Parking_Violations_Issued_-_Fiscal_Year_2014__August_2013___June_2014_.csv","/home/hedi/IdeaProjects/drone/csv/project/Parking_Violations_Issued_-_Fiscal_Year_2015.csv","/home/hedi/IdeaProjects/drone/csv/project/Parking_Violations_Issued_-_Fiscal_Year_2016.csv","/home/hedi/IdeaProjects/drone/csv/project/Parking_Violations_Issued_-_Fiscal_Year_2017.csv")
+
+    def runProducer(x: String): Any = {
+      val input = Source.fromFile(x).getLines()
       input.next()
       val producer = new Producertwo
       producer.produce(input)
 
+    }
+
+    @tailrec
+    def matchCsv(x: List[String]): Any = x match {
+      case head::tail => runProducer(head);matchCsv(tail)
+      case head::Nil => runProducer(head)
+      case _ => println("error")
+    }
+
+    matchCsv(listFile)
+
+
   }
 }
+
